f1671c9af904548417e3a09e89008b49ed310935
Mike Stagney <49824414+mikestagney@users.noreply.github.com>
2022-09-09 00:38:42 -0400

    Update RecipeController.java

--- RecipeController.java
+++ RecipeController.java
@@ -1,44 +1,57 @@
-package recipes;
+ackage recipes;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
+import javax.validation.*;
+import org.springframework.validation.annotation.Validated;
 
 @RestController
+@Validated
 public class RecipeController {
 
-    Recipe recipe;
-    Map<Integer, Recipe> recipeRepo;
+    @Autowired
+    RecipeService recipeService;
 
-    RecipeController() {
-        recipeRepo = new HashMap<>();
-
-    }
     @GetMapping("/api/recipe/{id}")
-    public ResponseEntity<Recipe> getRecipeNoID(@PathVariable int id) {
-        boolean isValid = recipeRepo.containsKey(id);
+    public ResponseEntity<Recipe> getRecipeNoID(@PathVariable Long id) {
+        Optional<Recipe> recipeOptional = recipeService.findById(id);
 
-        if (isValid) {
-            recipe = recipeRepo.get(id);
+        if (recipeOptional.isPresent()) {
+            Recipe recipe = recipeOptional.get();
             return new ResponseEntity<>(recipe, HttpStatus.OK);
         }
         else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
     @PostMapping("/api/recipe/new")
-    public ObjectNode postRecipe(@RequestBody Recipe newRecipe) {
-        recipe = new Recipe(newRecipe.name, newRecipe.description, newRecipe.ingredients, newRecipe.directions);
-        recipeRepo.put(recipe.id, recipe);
+    public ObjectNode postRecipe(@Valid @RequestBody Recipe newRecipe) {
+        Long id = recipeService.saveAndReturnId(newRecipe);
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode idResponse = mapper.createObjectNode();
-        idResponse.put("id", recipe.id);
+        idResponse.put("id", id);
         return idResponse;
+    }
+
+    @DeleteMapping("/api/recipe/{id}")
+    public ResponseEntity<Object> deleteRecipeByID(@PathVariable Long id) {
+        boolean recipeExists = recipeService.existsById(id);
+
+        if (recipeExists) {
+            recipeService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
