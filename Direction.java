af98787ee7f3a120b3b0de15cedc727ffac741cb
Mike Stagney <49824414+mikestagney@users.noreply.github.com>
2022-09-09 00:36:43 -0400

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -1,19 +1,29 @@
 package recipes;
 
 import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "directions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Direction {
-
+    // This class does nothing.  I could not get the jpa many to one relationship to work.
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "direction_id")
-    int id;
+    @JsonIgnore
+    private Long id;
 
     @Column(name = "direction_name")
-    String name;
+    private String name;
 
-
+    @ManyToOne()
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
 
 }
