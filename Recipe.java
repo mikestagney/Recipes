3be2976e6b2fe4f209ed0ab4360e99bcc08f2d3c
Mike Stagney <49824414+mikestagney@users.noreply.github.com>
2022-09-09 00:38:05 -0400

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -9,6 +9,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "recipe")
@@ -17,37 +20,31 @@ import java.util.List;
 @NoArgsConstructor
 public class Recipe {
 
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     @JsonIgnore
-    int id;
+    private Long id;
 
     @Column
-    String name;
+    @NotBlank
+    private String name;
 
     @Column
-    String description;
+    @NotBlank
+    private String description;
 
-    @OneToMany
-    @JoinColumn(name = "recipe_id", nullable = false)
-    public List<Ingredient> ingredients = new ArrayList<>();
+    //@OneToMany(mappedBy = "recipe")
+    @Column
+    @ElementCollection
+    @NotEmpty
+    private List<String> ingredients = new ArrayList<>();
 
-    @OneToMany
-    @JoinColumn(name = "recipe_id")
-    public List<Direction> directions = new ArrayList<>();
-
-    static int idCounter = 0;
-
-    Recipe(String name, String description, List<Ingredient> ingredients, List<Direction> directions) {
-        this.name = name;
-        this.description = description;
-        this.ingredients = ingredients;
-        this.directions = directions;
-        idCounter++;
-        id = idCounter;
-    }
-
+    //@OneToMany(mappedBy = "recipe")
+    @Column
+    @ElementCollection
+    @NotEmpty
+    private List<String> directions = new ArrayList<>();
 
 }
+
